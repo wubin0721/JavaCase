9a43927ca71c32805de224530d32101068f7bac9
吴斌 <619092454@qq.com>
2024-04-10 23:28:14 +0900

    app关掉后点击通知栏跳转指定网页

--- firebasedemo/src/main/java/com/demo/firebasedemo/MainActivity.java
+++ firebasedemo/src/main/java/com/demo/firebasedemo/MainActivity.java
@@ -1,5 +1,7 @@
 package com.demo.firebasedemo;
 
+import static android.provider.CalendarContract.CalendarCache.URI;
+
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.net.Uri;
@@ -22,6 +24,9 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.messaging.FirebaseMessaging;
 
+import java.util.Iterator;
+import java.util.Set;
+
 public class MainActivity extends AppCompatActivity {
 
     public static final String TAG ="MainActivity";
@@ -30,6 +35,31 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        /*
+        app关掉之后，firebase会把通知发去通知栏，点击通知栏会去到启动页，intent有平台发送通知的第
+        5项其他选项以key-value形式的信息，可以从以下代码拿取
+         */
+        Intent intent = getIntent();
+        Bundle extras = intent.getExtras();
+        if(extras !=  null){
+            Set<String> keySet = extras.keySet();
+            Iterator<String> iterator = keySet.iterator();
+            while (iterator.hasNext()) {
+                String key = iterator.next();
+                Object value = extras.get(key);
+                Log.d(TAG, "key:"+key + ",value:" + value);
+
+                //拿取想要的网址并跳转
+                if(key.equals("key")){
+                    Uri uri = Uri.parse(String.valueOf(value));
+                    Intent vs = new Intent(Intent.ACTION_VIEW,uri);
+                    startActivity(vs);
+                }
+            }
+        }
+
 
         firebaseInit();
 
@@ -50,9 +80,9 @@ public class MainActivity extends AppCompatActivity {
                         }
 
                         // Get new FCM registration token
-                        String token = task.getResult();
-                        Log.d(TAG, token);
-                        Toast.makeText(MainActivity.this, token, Toast.LENGTH_SHORT).show();
+//                        String token = task.getResult();
+//                        Log.d(TAG, token);
+//                        Toast.makeText(MainActivity.this, token, Toast.LENGTH_SHORT).show();
                     }
                 });
     }
--- firebasedemo/src/main/java/com/demo/firebasedemo/service/MessagingService.java
+++ firebasedemo/src/main/java/com/demo/firebasedemo/service/MessagingService.java
@@ -30,16 +30,6 @@ public class MessagingService extends FirebaseMessagingService {
         showNotificaiton(message);
     }
 
-    @Override
-    public void onDeletedMessages() {
-        super.onDeletedMessages();
-        /*
-        在某些情况下，FCM 可能不会传递消息。比如说，如果在特定设备连接到 FCM 时，您的应用在该设备上的待处理消息过多（超过 100 条），
-        或者设备超过一个月未连接到 FCM，就会发生这种情况。在这些情况下，您可能会收到对 FirebaseMessagingService.onDeletedMessages()
-        的回调。当应用实例收到此回调时，应该执行一次与应用服务器的完全同步。如果您在过去 4 周内未向该设备上的应用发送消息，FCM 将不会调用
-        onDeletedMessages()
-         */
-    }
 
     @Override
     public void onNewToken(@NonNull String token) {
