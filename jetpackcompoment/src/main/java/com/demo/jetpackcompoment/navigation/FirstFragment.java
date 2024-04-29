73e2e52231ac49500c3d696fa0fc42b0068d7cbc
吴斌 <619092454@qq.com>
2024-04-29 11:43:34 +0900

    添加Jetpack Navigation管理Fragment之间的跳转

---
+++ jetpackcompoment/src/main/java/com/demo/jetpackcompoment/navigation/FirstFragment.java
@@ -0,0 +1,37 @@
+package com.demo.jetpackcompoment.navigation;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+import com.demo.jetpackcompoment.R;
+
+
+public class FirstFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_first, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        Button btn1 = getView().findViewById(R.id.btn1);
+        btn1.setOnClickListener((v) -> {
+            NavController navController = Navigation.findNavController(v);
+            //指定当前fragmen的跳转路线
+            navController.navigate(R.id.action_firstFragment_to_secondFragment2);
+        });
+    }
+}
---
+++ jetpackcompoment/src/main/java/com/demo/jetpackcompoment/navigation/FragmentActivity.java
@@ -0,0 +1,25 @@
+package com.demo.jetpackcompoment.navigation;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.NavigationUI;
+
+import com.demo.jetpackcompoment.R;
+
+public class FragmentActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fragment);
+        //添加NavHostFragment，即fragment的容器
+        NavController navController = Navigation.findNavController(this,R.id.fragment);
+    }
+}
